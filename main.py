93f31048aa5382e0670b945de211c8effa227d4f
Benedikt Kau <40298136+benediktkau@users.noreply.github.com>
2020-09-28 22:18:14 +0100

    First Try
    Stable plotting of Covid19 Line plot

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
+import matplotlib.animation as ani
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
-plt.style.use('seaborn-pastel')
+import pandas as pd
+#import scipy
 
+# Read dataframe
+df = pd.read_csv('covid.csv', delimiter=',', header='infer')
+df_interest = df.loc[
+    df['Country/Region'].isin(['United Kingdom', 'US', 'Italy', 'Germany'])
+    & df['Province/State'].isna()]
+df_interest.rename(
+    index=lambda x: df_interest.at[x, 'Country/Region'], inplace=True)
+df1 = df_interest.transpose()
+df1 = df1.drop(['Province/State', 'Country/Region', 'Lat', 'Long'])
 
+df1 = df1.loc[(df1 != 0).any(1)]
+df1.index = pd.to_datetime(df1.index)
+
+df1 = df1.rolling(2, win_type='gaussian').sum(std=3)
+
+color = ['red', 'green', 'blue', 'orange']
 fig = plt.figure()
-ax = plt.axes(xlim=(0, 4), ylim=(-2, 2))
-line, = ax.plot([], [], lw=3)
+plt.xticks(rotation=45, ha="right", rotation_mode="anchor") #rotate the x-axis values
+plt.subplots_adjust(bottom = 0.2, top = 0.9) #ensuring the dates (on the x-axis) fit in the screen
+plt.ylabel('No of Deaths')
+plt.xlabel('Dates')
 
-def init():
-    line.set_data([], [])
-    return line,
-def animate(i):
-    x = np.linspace(0, 4, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
+def buildmebarchart(i=int):
+    plt.legend(df1.columns)
+    p = plt.plot(df1[:i].index, df1[:i].values) #note it only returns the dataset, up to the point i
+    for i in range(0,4):
+        p[i].set_color(color[i]) #set the colour of each curve
+import matplotlib.animation as ani
+animator = ani.FuncAnimation(fig, buildmebarchart, interval = 100)
+animator.save('animation.gif', dpi=300)
 
-anim = FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
-
-
-anim.save('sine_wave.gif', writer='imagemagick')
+#plt.show()
