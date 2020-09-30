6cdd46b5d132406e51d9747495f09c3b7093f292
Benedikt Kau <40298136+benediktkau@users.noreply.github.com>
2020-09-30 17:31:30 +0100

    First clean version

--- temperature.py
+++ temperature.py
@@ -3,20 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
 # new attempt with temperature
-df3 = pd.read_csv('temperature.csv', delimiter=',', header='infer', squeeze=True, infer_datetime_format=True, index_col=0)
-print(df3)
+df = pd.read_csv('temperature.csv', delimiter=',', header='infer', squeeze=True, infer_datetime_format=True, index_col=0)
 
-
-fig = plt.figure()
+fig, ax = plt.subplots()
 plt.ylabel('Degrees')
 plt.xlabel('Years')
-line.set_color("blue")
 
-def buildmebarchart(i):
-    p = plt.plot(df3[:i].index, df3[:i].values)
+def animate(i):
+    plt.plot(df[:i].index, df[:i].values, 'b')
 
 import matplotlib.animation as ani
-animator = ani.FuncAnimation(fig, buildmebarchart, interval = 100)
+animator = ani.FuncAnimation(fig, animate, interval = 50, frames=140)
 animator.save('temperature.gif', dpi=200)
