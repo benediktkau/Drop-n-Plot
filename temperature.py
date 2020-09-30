07369177ccdb995e3a43c33e96237df64b25fa75
Benedikt Kau <40298136+benediktkau@users.noreply.github.com>
2020-09-30 22:22:38 +0100

    First Line Plot Version with Smoothing

--- temperature.py
+++ temperature.py
@@ -6,7 +6,7 @@ import scipy as sc
 
 def main():
 
-    font = {'family': 'sans-serif',
+    font = {'family': 'Verdana',
             'color':  'black',
             'weight': 'normal',
             'size': 10,
@@ -17,28 +17,32 @@ def main():
     df.index = pd.to_datetime(df.index, format='%Y')
     
     # Smoothening
-    df_resampled = df.resample('Q').mean() #add months to data
-    
-    df_interpolated = df_resampled.interpolate(method='cubic')
-    df = df_interpolated
-    print(df_interpolated)
-    
-    
-    
+    smootheningMethod = 'cubic'
+    df = smoothening(df, smootheningMethod)
 
+    # Layout
     fig, ax = plt.subplots()
     plt.ylabel('Degrees',fontdict=font)
     plt.xlabel('Years',fontdict=font)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.spines['left'].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.get_xaxis().tick_bottom()  
+    ax.get_yaxis().tick_left() 
 
     def animate(i):
         plt.plot(df[:i].index, df[:i].values, 'cornflowerblue')
-
+ 
     import matplotlib.animation as ani
     animator = ani.FuncAnimation(fig, animate, interval = 50, frames=df.size-1)
     animator.save('temperature.gif', dpi=200)
 
-
+def smoothening(df, method):
+    # https://machinelearningmastery.com/resample-interpolate-time-series-data-python/
+    df_resampled = df.resample('Q').mean() #add months to data
+    df_interpolated = df_resampled.interpolate(method=method)
+    df = df_interpolated
+    return df_interpolated
+    
 main()
