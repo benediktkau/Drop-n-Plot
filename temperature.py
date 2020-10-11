26f6e74e64dca804d564d1a1ae0e7b8c960de36e
Benedikt Kau <40298136+benediktkau@users.noreply.github.com>
2020-10-11 16:00:15 +0100

    Created Web App Files
    not stable

--- temperature.py
+++ temperature.py
@@ -1,54 +1,73 @@
 import matplotlib.animation as ani
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import scipy as sc
+
 
 def main():
-
     font = {'family': 'Verdana',
-            'color':  'black',
             'weight': 'normal',
             'size': 8,
             'color': 'grey'
             }
 
-    # new attempt with temperature
-    df = pd.read_csv('temperature.csv', delimiter=',', header='infer', squeeze=True, index_col=0)
-    df.index = pd.to_datetime(df.index, format='%Y')
-    
+    timeInterval = 'day'
+    filename = 'covid.csv'
+
+    df, dfType = readData(filename)
+
+    df.plot()
+    plt.show()
+    return
+
     # Smoothening
     smootheningMethod = 'cubic'
     df = smoothening(df, smootheningMethod)
 
+    return
+
     # Layout
     fig, ax = plt.subplots()
-    plt.ylabel('Degrees',fontdict=font, rotation='horizontal', verticalalignment='baseline', loc='top')
-    #plt.xlabel('Years',fontdict=font)
+    plt.ylabel('Degrees', fontdict=font, rotation='horizontal', loc='top')
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.spines['left'].set_visible(False)
     ax.spines["bottom"].set_visible(False)
     plt.xticks(fontsize=8, color='grey')
     plt.yticks(fontsize=8, color='grey')
-    plt.plot(df.index, df.values, 'cornflowerblue')
-    plt.show
-    plt.savefig('chart.png')
-    return
+    ax.tick_params(axis='x', colors='grey')
+    ax.tick_params(axis='y', colors='grey')
 
+    # plt.show
+    # plt.savefig('chart.png')
+    # return 
+
+    # Animation
     def animate(i):
-        plt.plot(df[:i].index, df[:i].values, 'cornflowerblue')
+        plt.plot(df[:i].index, df[:i].values, 'darkorange')
         plt.title('Temperature \n' + str(df.index[i].strftime('%Y')))
- 
-    import matplotlib.animation as ani
-    animator = ani.FuncAnimation(fig, animate, interval = 50, frames=df.size-1)
+
+    animator = ani.FuncAnimation(fig, animate, interval=50, frames=df.size - 1)
     animator.save('temperature.gif', dpi=200)
+
+
+def readData(filename):
+    df = pd.read_csv(filename, delimiter=',', header='infer', squeeze=True, index_col=0, parse_dates=True)
+    if isinstance(df, pd.Series):
+        df.index = pd.to_datetime(df.index, format='%Y')
+        dfType = 'Series'
+    else:
+        # df.transpose()
+        print(df)
+
+        dfType = 'DataFrame'
+    return df, dfType
+
 
 def smoothening(df, method):
     # https://machinelearningmastery.com/resample-interpolate-time-series-data-python/
-    df_resampled = df.resample('Q').mean() #add months to data
+    df_resampled = df.resample('Q').mean()  # add months to data
     df_interpolated = df_resampled.interpolate(method=method)
-    df = df_interpolated
     return df_interpolated
-    
+
+
 main()
