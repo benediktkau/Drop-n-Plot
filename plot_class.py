71030a7808894817f0322245df7a07902bc44617
Benedikt Kau <40298136+benediktkau@users.noreply.github.com>
2020-12-27 14:25:38 +0100

    plotting multiple works

--- plot_class.py
+++ plot_class.py
@@ -7,9 +7,13 @@ import pandas as pd
 import datetime
 import helpers
 import numpy as np
+import random
 
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = {'txt', 'csv'}
+COLORS = (
+'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive',
+'tab:cyan')
 
 logging.basicConfig(filename='dropnplot_log',
                     level=logging.INFO,
@@ -37,17 +41,26 @@ class Plot:
         timeInterval = 'day'
 
         try:
-            df, datetime_index = self.read_dataframe(self.filepath)
+            df, datetime_index, df_type = self.read_dataframe(self.filepath)
         except FileNotFoundError as e:
             logging.critical('File not found', e)
-            exit(1)
 
         # Smoothening
         interpolation_method = 'cubic'
 
-        if datetime_index: # interpolation for panda datetime index
-            df = self.datetime_interpolation(df, interpolation_method)
-        else: # standard interpolation
+        if datetime_index:  # interpolation for panda datetime index
+            datetime_details_dict = self.get_datetime_index_details(df.index)
+
+            datetime_xlim_date = min(df.index) + pd.Timedelta(weeks=int((datetime_details_dict['years'] * 52 +
+                                                                         datetime_details_dict['months'] * 4.3 +
+                                                                         datetime_details_dict['weeks']) * 0.1),
+                                                              days=int(datetime_details_dict['days'] * 0.1),
+                                                              hours=int(datetime_details_dict['hours'] * 0.1),
+                                                              minutes=int(datetime_details_dict['minutes'] * 0.1),
+                                                              seconds=int(datetime_details_dict['seconds'] * 0.1))
+
+            df = self.datetime_interpolation(df, datetime_details_dict['datetime_freq'], interpolation_method)
+        else:  # standard interpolation
             df = self.standard_interpolation(df, interpolation_method)
             df.reset_index(drop=True, inplace=True)
 
@@ -55,25 +68,35 @@ class Plot:
 
         # Get xlim, ylim
         if datetime_index:
-            xlim_init = (min(df.index), max(df.index))
-            ylim_init = (min(df.values), max(df.values))
+            xlim_init = (min(df.index), max(df.truncate(after=datetime_xlim_date).index))
+
         else:
             xlim_init = (0.1 * min(df.index), 0.1 * max(df.index))
-            ylim_init = (0, 0.3 * max(df.values))
+
+        ylim_init = (0, 0.3 * df.values.max())
 
         fig = plt.pyplot.figure()
         ax = plt.pyplot.axes(xlim=xlim_init, ylim=ylim_init)
-        line, = ax.plot([], [], lw=2)
+        lines = []
+
+        if df_type != "Series":
+            for column in df:
+                color = COLORS[random.randint(0, len(COLORS) - 1)]
+                print(color)
+                line_artist = ax.plot([], [], lw=2, color=color)[0]
+                lines.append(line_artist)
 
         def init():
             """
 
             :return:
             """
-            line.set_data([], [])
-            return line,
 
-        plt.pyplot.ylabel('Degrees', fontdict=font, rotation='horizontal', loc='top')
+            for line in lines:
+                line.set_data([], [])
+            return lines
+
+        # plt.pyplot.ylabel('Degrees', fontdict=font, rotation='horizontal', loc='top')
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
         ax.spines['left'].set_visible(False)
@@ -89,25 +112,30 @@ class Plot:
         # Animation
         def animate(i):
             """
+            Animation function for matplotlib FuncAnimation
 
-            :param i:
-            :return:
+            :param i: index to iterate over df
+            :return: line element to be plotted
             """
 
-            # plt.pyplot.legend(df.columns)
-            # plt.pyplot.plot(df[:i].index, df[:i].values, 'darkorange')
-
             x = list(df.index[:i])
-            y = df.values[:i]
 
-            line.set_data(x, y)
+            for column, line in enumerate(lines):
+                y = df[df.columns[column]][:i]
+                line.set_data(x, y)
 
             # Adapt ylim, xlim
-            if not len(x) == 0 or not len(y) == 0:
-                if df.index[i] < xlim_init[0] or df.index[i] > xlim_init[1]:
-                    ax.set_xlim(0, max(x) * 1.01)
+            if not len(x) == 0 or not len(y) == 0:  # do not adapt if line is still empty
 
-                if df.values[i] < ylim_init[0] or df.values[i] > ylim_init[1]:
+                # If plotting progressed beyond initial xlim, extent axis by new maximum
+                if df.index[i] < xlim_init[0] or df.index[i] > xlim_init[1]:
+                    if datetime_index:
+                        ax.set_xlim(min(df.index), max(df[:i].index))
+                    else:
+                        ax.set_xlim(0, max(x) * 1.01)
+
+                # If plotting progressed beyond initial ylim, extent axis by new maximum
+                if min(df.values[i]) < ylim_init[0] or max(df.values[i]) > ylim_init[1]:
                     ax.set_ylim(min(y), max(y) * 1.1)
 
             # print progress bar
@@ -119,7 +147,7 @@ class Plot:
             else:
                 plt.pyplot.title('\n'.join([plotTitle, str(int(df.index[i]))]))
 
-            return line,
+            return lines
 
         filename_plot = self.create_filename()
 
@@ -150,7 +178,7 @@ class Plot:
 
         # Read Dataframe or Series
         df = pd.read_csv(filename, delimiter=delimiter, header='infer', squeeze=True,
-                         index_col=0 , parse_dates=True)
+                         index_col=0, parse_dates=True)
 
         # Check whether index is pd.datetime format
         if isinstance(df.index, pd.DatetimeIndex):
@@ -159,16 +187,38 @@ class Plot:
         else:
             datetime_index = False
 
+        if len(df.index) < len(df.columns):
+            df.transpose()
 
         if isinstance(df, pd.Series):
             # df.index = pd.to_datetime(df.index, format='%Y')
-            dfType = 'Series'
+            df_type = 'Series'
         else:
-            df = df.transpose()
             # df.index = pd.to_datetime(df.index)
-            dfType = 'DataFrame'
+            df_type = 'DataFrame'
 
-        return df, datetime_index
+        return df, datetime_index, df_type
+
+    @staticmethod
+    def get_datetime_index_details(index) -> dict:
+
+        datetime_details_dict = {'years': max(index).year - min(index).year,
+                                 'months': max(index).month - min(index).month,
+                                 'weeks': max(index).week - min(index).week,
+                                 'days': max(index).day - min(index).day,
+                                 'hours': max(index).hour - min(index).hour,
+                                 'minutes': max(index).minute - min(index).minute,
+                                 'seconds': max(index).second - min(index).second}
+
+        # Determine the largest unit >0 to be used in the interpolation method
+        for key in datetime_details_dict:
+            if datetime_details_dict[key] > 0:
+                datetime_details_dict['datetime_freq'] = key
+                break
+
+        print(datetime_details_dict)
+
+        return datetime_details_dict
 
     @staticmethod
     def create_filename():
@@ -205,22 +255,36 @@ class Plot:
         return df_interpolated
 
     @staticmethod
-    def datetime_interpolation(df, method):
+    def datetime_interpolation(df, datetime_freq, method):
         """
 
+        :param datetime_freq:
         :param df:
         :param method:
         :return:
         """
 
+        # Dictionary to determine interpolation frequency
+        datetime_interpolation_dict = {
+            'years': 'Q',
+            'quarters': 'M',
+            'months': 'W',
+            'weeks': 'D',
+            'days': 'H',
+            'hours': 'T',
+            'minutes': 'S',
+            'seconds': 'L',
+        }
+
+        interpolation_freq = datetime_interpolation_dict[datetime_freq]
+        logging.debug('Interpolation Frequency:' + interpolation_freq)
+
         # https://machinelearningmastery.com/resample-interpolate-time-series-data-python/
-        df_resampled = df.resample('Q').mean()  # add quarters to data
+        df_resampled = df.resample(interpolation_freq).mean()  # add quarters to data
         df_interpolated = df_resampled.interpolate(method=method)
         return df_interpolated
 
 
 if __name__ == '__main__':
-    new_plot = Plot('src/temperature.csv')
-    # new_plot.new()
-
+    new_plot = Plot('src/attempt.csv')
     new_plot.main('new plot on christmas')
