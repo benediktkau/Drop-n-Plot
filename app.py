e4f1b80dff7f4827f250182d86263159ca8246d6
Benedikt Kau <40298136+benediktkau@users.noreply.github.com>
2020-12-30 18:16:33 +0100

    logging

--- app.py
+++ app.py
@@ -62,4 +62,7 @@ def plot():
         return render_template("plot.html", plotFilename='../static/temperature.gif')
 
 if __name__ == '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     app.run(debug=True)
